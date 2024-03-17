872cfdf8848d8ca5f1e7cae9e7fc9a07bb4a955a
Tim Amburn <91898542+TimA314@users.noreply.github.com>
2024-03-17 08:18:37 -0700

    add zustand and create login component

--- NetCard/src/components/Login.tsx
+++ NetCard/src/components/Login.tsx
@@ -1,3 +1,5 @@
+import { Key } from '@mui/icons-material';
+import { Box, Button, InputAdornment, TextField } from '@mui/material';
 import React, { useState } from 'react';
 
 const Login = () => {
@@ -5,20 +7,30 @@ const Login = () => {
 
   const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault();
-
     console.log("Private Key:", privateKey);
   }
-  
+
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="text"
-        placeholder="Private Key"
-        value={privateKey}
-        onChange={(e) => setPrivateKey(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <Box 
+      component="form"
+      autoComplete="off"
+      onSubmit={handleLogin}>
+        <TextField
+          id="privateKey"
+          placeholder="Private Key"
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Key />
+              </InputAdornment>
+            ),
+          }}
+          value={privateKey}
+          onChange={(e) => setPrivateKey(e.target.value)}
+        />
+        <Button type="submit">Login</Button>
+    </Box>
   );
 };
 
